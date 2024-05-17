ac2f53acc67690770eeca85dd7718675d7eb643c
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-05-17 21:24:18 +0530

    contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,25 +5,37 @@ const Contact = () => {
   const [Email, setEmail] = useState("");
   const [Message, setMessage] = useState("");
 
-  function submit() {
-    alert("done");
+  async function submit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    const url =
+      "https://script.google.com/macros/s/AKfycbzgEhCAHclkTJ4mQJGRVSd_yuZhE2Q4-4Tylr66eTbjRrLUAFfT80EUBM0a155UcGK0/exec";
+
+    formData.append("Name", Name);
+    formData.append("Email", Email);
+    formData.append("Message", Message);
+
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      body: formData,
+    });
   }
 
   return (
     <div className="flex gap-x-10 mb-10">
-      <div className="block mb-2 text-[24px] text-gray-900 dark:text-white w-2/5 break-words mt-[75px]">
+      <div className="block mb-2 text-[24px] text-dimWhite w-2/5 break-words mt-[75px]">
         Unlock the potential of AI for your business today! Take the first step
         towards enhanced efficiency and innovation by filling up the form on the
         side. Plus, as a bonus, enjoy a complimentary consultation with our
         expert team at NexaWings. Let's turn your AI dreams into
         reality – together!
       </div>
-      <form action="/" className="flex flex-col gap-y-6 w-3/5">
+      <form onSubmit={(e) => submit(e)} className="flex flex-col gap-y-6 w-3/5">
         <div>
-          <label class="block mb-2 text-[24px] text-gray-900 dark:text-white">
-            Name
-          </label>
+          <label className="block mb-2 text-[24px] text-white">Name</label>
           <input
+            name="Name"
             required
             type="text"
             placeholder="Name"
@@ -32,10 +44,9 @@ const Contact = () => {
           />
         </div>
         <div>
-          <label class="block mb-2 text-[24px] text-gray-900 dark:text-white">
-            Email
-          </label>
+          <label className="block mb-2 text-[24px] text-white">Email</label>
           <input
+            name="Email"
             required
             type="text"
             placeholder="Email"
@@ -44,10 +55,9 @@ const Contact = () => {
           />
         </div>
         <div>
-          <label class="block mb-2 text-[24px] text-gray-900 dark:text-white">
-            Message
-          </label>
+          <label className="block mb-2 text-[24px] text-white">Message</label>
           <textarea
+            name="Message"
             required
             rows={5}
             type="text"
@@ -59,7 +69,6 @@ const Contact = () => {
 
         <button
           type="submit"
-          onClick={submit}
           className="py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] max-w-[150px] text-primary outline-none rounded-[10px] mt-5"
         >
           Submit
